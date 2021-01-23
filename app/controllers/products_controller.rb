5b034cdf1b9e76f007cf87c4b63896617bcbe673
Marcus Pereira <marcus.work@hotmail.com>
2021-01-23 11:25:05 -0300

    Search products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
   def index
     if params[:term].present?
-      @products = Product.search("%#{params[:term]}%")
+      # @products = Product.search("%#{params[:term]}%")
+      @products = Product.where(name: /#{params[:term]}/i)
     else
       @products = Product.all
     end
