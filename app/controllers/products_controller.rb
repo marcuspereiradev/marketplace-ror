f33089e8f6c1c8f0be5f0b3a7997408457a6c926
Marcus Pereira <marcus.work@hotmail.com>
2021-01-23 12:01:55 -0300

    Config search products and environment variable to production

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
   def index
     if params[:term].present?
-      @products = Product.search("%#{params[:term]}%")
+      # @products = Product.search("%#{params[:term]}%") This line just work in development. Elasticsearch
+      @products = Product.where(name: /#{params[:term]}/i)
     else
       @products = Product.all
     end
